9237ee5251ac426fc37f61a799224d943920cbfb
Freewayz <alfredemmanuel2018@yahoo.com>
2021-08-13 17:01:14 +0100

    custom hooks

---
+++ custm-hookks/src/App.js
@@ -0,0 +1,13 @@
+import BackwardCounter from './components/BackwardCounter';
+import ForwardCounter from './components/ForwardCounter';
+
+function App() {
+  return (
+    <>
+      <ForwardCounter />
+      <BackwardCounter />
+    </>
+  );
+}
+
+export default App;
