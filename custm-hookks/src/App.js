3ce81884f8b148eb4d18b5fd3818c090e8a978aa
Freewayz <alfredemmanuel2018@yahoo.com>
2021-08-14 00:26:47 +0100

    completed the custom hook section

--- custm-hookks/src/App.js
+++ custm-hookks/src/App.js
@@ -2,38 +2,25 @@ import { useEffect, useState } from 'react';
 
 import Tasks from './components/Tasks/Tasks';
 import NewTask from './components/NewTask/NewTask';
+import useFetch from './hooks/useFetch';
 
 function App() {
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
   const [tasks, setTasks] = useState([]);
 
-  const fetchTasks = async (taskText) => {
-    setIsLoading(true);
-    setError(null);
-    try {
-      const response = await fetch(
-        'https://dummy-fdf2b-default-rtdb.firebaseio.com/tasks.json'
-      );
+  const transformedTasks = tasksObj => {
+    const loadedTasks = [];
 
-      if (!response.ok) {
-        throw new Error('Request failed!');
-      }
-
-      const data = await response.json();
-
-      const loadedTasks = [];
-
-      for (const taskKey in data) {
-        loadedTasks.push({ id: taskKey, text: data[taskKey].text });
-      }
-
-      setTasks(loadedTasks);
-    } catch (err) {
-      setError(err.message || 'Something went wrong!');
+    for (const taskKey in tasksObj) {
+      loadedTasks.push({ id: taskKey, text: tasksObj[taskKey].text });
     }
-    setIsLoading(false);
-  };
+
+    setTasks(loadedTasks);
+  }
+
+  const { isLoading, error, sendRequest: fetchTasks } = useFetch(
+    {url: 'https://dummy-fdf2b-default-rtdb.firebaseio.com/tasks.json'},
+    transformedTasks
+  );
 
   useEffect(() => {
     fetchTasks();
