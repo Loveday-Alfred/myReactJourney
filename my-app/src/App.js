ac496fc204409f5b84fc88fe9ce276ee8b5b59e4
Freewayz <alfredemmanuel2018@yahoo.com>
2021-08-19 16:50:01 +0100

    more on axios

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,59 @@
 import './App.css';
+import axios from 'axios';
+import {useState, useEffect} from 'react';
+
+const client = axios.create({ 
+  baseURL: "https://jsonplaceholder.typicode.com/posts"
+});
 
 function App() {
+  const [post, setPost] = useState(null);
+
+  useEffect(() => {
+    async function getPost() {
+      const response = await client.get("/1");
+      setPost(response.data);
+    }
+     getPost();
+  }, []);
+
+  function createPost() {
+    client
+      .post("/", {
+        title: "Hello World",
+        body: "New Post"
+      })
+      .then((response) => {
+        setPost(response.data);
+      });
+  }
+
+  // function updatedPost() {
+  //   client
+  //     .put("/1", {
+  //       title: "Hello World",
+  //       body: "Updated Post"
+  //     })
+  //     .then((response) => {
+  //       setPost(response.data);
+  //     });
+  // }
+
+  // async function deletePost() {
+  //   await client
+  //     .delete("/1")
+  //       alert("Post Deleted");
+  //       setPost(null);
+  // }
+
+  // if(error) return `Error:${error.message}`;
+  if(!post) return "No Post!";
+
   return (
     <div className="App">
-      <h1>Hello Axios</h1>
+      <h1>{post.title}</h1>
+      <p>{post.body}</p>
+      <button onClick={createPost}>Create Post</button>
     </div>
   );
 }
