fb1f5033c545a5161a6385b72191dd91cb1964f7
Freewayz <alfredemmanuel2018@yahoo.com>
2021-08-19 21:41:54 +0100

    more axios

--- random-gifs/src/components/Random.js
+++ random-gifs/src/components/Random.js
@@ -1,6 +1,16 @@
+import { useEffect } from "react";
+import axios from "axios";
+
+// API Key 
 const API_KEY = process.env.REACT_APP_API_KEY;
 
 const Random = () => {
+
+    useEffect(() => {
+        const url = `https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}`;
+        axios.get(url);
+    }, []);
+
     return (
         <div>
             <h1>Random</h1>
