68686182165d03524b9e826cf6a96462ed996439
Freewayz <alfredemmanuel2018@yahoo.com>
2021-08-21 01:15:31 +0100

    extracting routes

--- routing-routing/src/pages/ProductDetails.js
+++ routing-routing/src/pages/ProductDetails.js
@@ -1,7 +1,14 @@
+import { useParams } from "react-router-dom";
+
 const ProductDetails = () => {
+    const params = useParams();
+
+    console.log(params.productId);
+
     return (
         <section>
             <h1>Product Details</h1>
+            <p>{params.productId}</p>
         </section>
     )
 }
